7eb7fe605a28bc9565c23e4104bc7e5e1d1c79be
Simon Hampton <hotbelgo@gmail.com>
2015-07-26 17:59:06 +0200

    better extension handling

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Angular-HTTP-Server
 ## Simple HTTP server for developing sites that use client side routing
 
-Very simple server that returns 'index.html' for any route, or the file if it exists
+Very simple server that returns a file if it exists, or 'index.html' otherwise (i.e. when the url is a route).
+
+Other http-servers give a 404 when you try to refresh a page that is a route on a client side app
+
+To use: `angular-http-server` from the root directory you want to serve, and turn your browser to `localhost:8080`.
--- angular-http-server.js
+++ angular-http-server.js
@@ -12,9 +12,14 @@ var server = http.createServer(function (req, res) {
         var fileBuffer;
 
         if (!err && stats.isFile()) {
+
+            var fileExtension = possibleFilename.split('.');
+            // we need last part of array in case of e.g. xxxx.min.css
+            fileExtension = fileExtension[fileExtension.length - 1];
+
             console.log("Sending file: %s", possibleFilename);
             fileBuffer = fs.readFileSync(possibleFilename);
-            res.writeHead(200, { 'Content-Type': 'text/css' });
+            res.writeHead(200, { 'Content-Type': 'text/'+fileExtension });
         }
         else {
             console.log("Route %s, replacing with index.html", possibleFilename);
@@ -26,4 +31,5 @@ var server = http.createServer(function (req, res) {
         res.end();
     });
 });
-server.listen(8000, function () { return console.log("Listening"); });
+
+server.listen(8080, function () { return console.log("Listening on 8080"); });
